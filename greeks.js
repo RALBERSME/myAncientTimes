f8db7e75a2889fcba87bb0a5cac4b203925ee382
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-08-25 09:50:41 +0200

    third commit

---
+++ greeks.js
@@ -0,0 +1,211 @@
+const moreDataArray = [
+  `<p>
+                His studies cover the natural sciences, philosophy, linguistics,
+                economics, politics, psychology, and the arts.
+              </p>
+              <p></p>
+              <p>
+                Aristotle spent some twenty years advancing his education in
+                Egypt until the invasion of Alexander the Great in 332 BCE, when
+                Aristotle got the opportunity he had been praying for, to
+                ransack leading Egyptian libraries and carry off all the books
+                he wanted, to found his academy in Athens called <br />Source &
+                for additional reading:
+              </p><br>
+              <a
+                href="https://www.modernghana.com/news/438885/aristotle-a-graduate-of-the-african-mystery-school.html"
+                target="_blank"
+                >click here to read: <br>Aristotle, A Graduate Of The African Mystery School
+              </a>`,
+  `<p>
+              He was a leader in metaphysics, epistemology, ethics,
+              anthropology, political theory, cosmology, art theory and
+              philosophy of language.
+            </p>
+            <p>
+              Plato studied in Egypt for years. He spent months studying
+              arithmetic, music and number theory, as well as astronomy with the
+              temple priests in Memphis.
+            </p>
+            <p>
+              The knowledge gained from Egypt is also evident in his later work
+              on music theory (table in the “Nomoi” (Laws)). Friedrich Wilhelm
+              Korff investigated the Egyptian measurement and measurement
+              system, which had been introduced by the architect Imhotep under
+              Pharaoh Djoser around 2635 BC, and found that the cubit and
+              handbreadth measurements were only made up of the products of the
+              first five prime numbers (1, 2, 3, 5, 7), just like Plato's 60
+              parts of the number 5040 in the table of Nomoi, because 5040 or
+              factorial of seven (7!) is equal to the product of the first seven
+              natural numbers (1 × 2 × 3 × 4 × 5 × 6 × 7 = 5040). "Korff
+              suspected that Plato's table contained a pyramid builder's rule
+              and this was confirmed when he examined the dimensions of 29 large
+              pyramids in the manuals on pyramid construction and found that the
+              bases were almost always whole numbers, formed from the products
+              of these same numbers.<br />The unity of the fields of knowledge
+              arithmetic, geometry and music theory were probably crucial for
+              the buildings in ancient Egypt.<br />Source & for additional
+              reading:
+            </p><br><br>
+            <a
+              href="https://ichsagmal.com/pyramidenklange-platon-korff-und-die-universalbildung-im-alten-agypten/"
+              target="_blank"
+              >Pyramidenklänge: Platon, Korff und die Universalbildung im alten
+              Ägypten
+            </a>
+            <br><br>
+            <a
+              href="https://socratesjourney.org/plato-in-egypt-the-greatest-philosopher-3/"
+              target="_blank"
+              >Plato in Egypt – The Greatest Philosopher
+            </a>
+            
+            `,
+  ` <p>
+              He was not only a philosopher, but also a mathematician and
+              founder of an influential religious-philosophical movement.
+            </p>
+            <p>
+              He travelled to Egypt, learned about religion and philosophy from
+              the Egyptian priests, and then introduced Egyptian religious ideas
+              to the Greeks.
+            </p>
+            <p>
+              Around seven hundred years or so after Pythagoras’s death, the
+              early Christian writer Klemes of Alexandria (lived c. 150 – c. 215
+              CE) wrote a miscellaneous philosophical work titled Stromata.
+              Klemes makes several noteworthy statements about Pythagoras: “And
+              Pythagoras is reported to have been a disciple of Sonches the
+              Egyptian arch-prophet".<br />Diogenes Laërtios, the Phoinikian
+              Neoplatonic philosopher Porphyrios of Tyre (lived c. 234 – c. 305
+              CE) wrote his own biography of Pythagoras He repeats the classic
+              stories about Pythagoras studying philosophy with the Egyptians,
+              Chaldaians, and Magoi, but adds the additional claim that
+              Pythagoras also studied among the Phoinikians. Source & for
+              additional reading: <br />
+            </p><br><br>
+            <a
+              href="https://talesoftimesforgotten.com/2021/03/24/did-pythagoras-study-philosophy-in-egypt/"
+              target="_blank"
+              >Did Pythagoras Study Philosophy in Egypt?
+            </a>`,
+  `<p>
+              He was a pre-Socratic natural philosopher, geometer and astronomer
+              of archaic Greece.
+            </p>
+            <p>
+              Thales was considered one of the Seven Sages and the founder of
+              ancient natural philosophy, astronomy and geometry.
+            </p>
+            <p>
+              Several ancient authors assume that Thales, at one point in his
+              life, visited Egypt, where he learned about geometry. It is
+              considered possible that Thales visited Egypt, since Miletus had a
+              permanent colony there (namely Naucratis). It is also said Thales
+              had close contacts with the priests of Thebes who instructed him,
+              or even that he instructed them in geometry. It is also possible
+              Thales knew about Egypt from accounts of others, without actually
+              visiting it.Source & for additional reading:
+            </p><br><br>
+            <a
+              href="https://en.wikipedia.org/wiki/Thales_of_Miletus"
+              target="_blank"
+              >Thales of Miletus - Wikipedia
+            </a><br>
+            <br />
+            <a
+              href="https://books.google.de/books?id=ld8lBAAAQBAJ&q=thales&pg=PA33&redir_esc=y#v=snippet&q=thales&f=false"
+              target="_blank"
+              >The Forgotten Revolution: How Science Was Born in 300 BC and Why
+              it Had to Be Reborn by Lucio Russo, 2004
+            </a>`,
+  `<p>
+              He is considered one of the most outstanding figures in the
+              history of medicine.
+            </p>
+            <p>
+              Hippocrates (the "father of medicine"), Herophilos, Erasistratus
+              and later Galen studied at the temple of Amenhotep, and
+              acknowledged the contribution of ancient Egyptian medicine to
+              Greek medicine.
+            </p><br>
+            <br />
+            <a
+              href="https://en.wikipedia.org/wiki/Ancient_Egyptian_medicine"
+              target="_blank"
+              >Ancient Egyptian medicine - Wikipedia
+            </a>
+            <br />
+            <a
+              href="https://www.vi-mm.eu/2021/08/16/medical-recipes-of-hippocrates-discovered-at-ancient-library-in-egypt/"
+              target="_blank"
+              >Medical Recipes of Hippocrates Discovered at Ancient Library in
+              Egypt
+            </a>`,
+  `<p>
+               As a philosopher, he was the last significant representative of the younger Stoicism. Freedom and justice, especially in the sense of equal rights for all, were among the political principles that Marcus Aurelius acquired early on and always propagated. According to his own statement, what was important to him was a rational and common good-oriented use of power in the Hellenistic philosophical tradition, which took into account the limits of its own competence and gave priority to greater expertise or delegated the problem-solving. </p>
+              <p><br><br></p>
+              <p>
+                <strong>Among his inner circle was Arnuphis (ca. 174 AD), an Egyptian general believed to have magical powers</strong>, who saved Marcus Aurelius' army from defeat by the Quadi (a Germanic tribe) when they were trapped in a pass blocked by the enemy. According to the writer Dio Cassius, Aurelius' troops were dying of thirst when Arnuphis conjured up a miraculous storm that confused the enemy and allowed the Romans to quench their thirst and win the battle. Their triumph became known as "The Miracle of the Thundering Legion," although Christians attributed the victory to their own prayers rather than to the help of Mercury and other gods invoked by Arnuphis.
+              </p><br>
+              <a
+                href="https://www.encyclopedia.com/science/encyclopedias-almanacs-transcripts-and-maps/arnuphis-ca-174-ce"
+                target="_blank"
+                >click here to read: <br>Arnuphis (ca. 174 C.E.) - Encylopedia
+              </a>`,
+  `<p>
+               He was a Roman philosopher, statesman, orator, and tragedian. He was Rome’s leading intellectual figure in the mid-1st century AD. </p>
+              <p><br><br></p>
+              <p>
+                Seneca’s health suffered when he was still young. Therefore he went to recuperate in Egypt, where his aunt lived with her husband, the prefect, Gaius Galerius. Returning to Rome about the year 31, he began a career in politics and law. 
+              </p><br>
+              <a
+                href="https://www.britannica.com/biography/Lucius-Annaeus-Seneca-Roman-philosopher-and-statesman/Stature-and-influence"
+                target="_blank"
+                >click here to read: <br>Seneca - Britannica
+              </a>`,
+  `<p>
+               He was a statesman, lawyer, scholar, philosopher, writer and Academic skeptic, who tried to uphold optimate principles during the political crises that led to the establishment of the Roman Empire. </p>
+              <p><br><br></p>
+              <p>
+                His extensive writings include treatises on rhetoric, philosophy and politics. He is considered one of Rome's greatest orators and prose stylists and the innovator of what became known as "Ciceronian rhetoric".
+              </p><br>
+              <a
+                href="https://en.wikipedia.org/wiki/Cicero"
+                target="_blank"
+                >click here to read: <br> Cicero - Wikipedia
+              </a><br><br>
+              <a
+                href="https://tulliana.eu/bibliografia/cicero-in-egypt-the-ciceronian-papyri-and-the-teaching-of-latin-in-the-east/"
+                target="_blank"
+                >click here to read: <br>« Cicero in Egypt: The Ciceronian Papyri and the Teaching of Latin in the East »
+              </a><br><br><a
+                href="https://www.philipharland.com/Blog/2023/02/egyptians-cicero-on-superstition-and-animal-worship-mid-first-century-bce/"
+                target="_blank"
+                >click here to read: <br>Egyptians: Cicero on superstition and animal-worship (mid-first century BC)
+              </a><br><br><a
+                href="https://www.philipharland.com/Blog/2023/02/egyptian-wisdom-cicero-diodoros-and-valerius-maximus-on-pythagoras-and-platos-supposed-journey-to-egypt-first-centuries-bce-and-ce/"
+                target="_blank"
+                >click here to read: <br>Egyptian wisdom: Cicero, Diodoros and Valerius Maximus on Pythagoras’ and Plato’s supposed journeys to Egypt 
+              </a><br><br><a
+                href="https://archiv.ub.uni-heidelberg.de/propylaeumdok/4659/1/Stadler_Aegyptenrezeption_in_der_roemischen_Kaiserzeit_2017.pdf"
+                target="_blank"
+                >click here to read: <br>Ägyptenrezeption in der römischen Kaiserzeit - in German
+              </a><br><br>
+              `,
+];
+const showMoreIds = [
+  "moreAristotle",
+  "morePlaton",
+  "morePythagoras",
+  "moreThales",
+  "moreHippocrates",
+  "moreAurelius",
+  "moreSeneca",
+  "moreCicero",
+];
+
+function showMore(num) {
+  console.log("showMore num is", num, typeof num);
+  document.getElementById(showMoreIds[num]).innerHTML = moreDataArray[num];
+}
